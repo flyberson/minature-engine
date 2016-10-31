fbb60ef5bf6c9ae81b52fbb2e46595f4c34827d4
Simon Rasmussen <simon@huebanden.dk>
2016-10-31 10:28:15 +0100

    test

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -15,6 +15,8 @@ public class Gittest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("yoyo");    }
-    
+        System.out.println("yoyo");
+        System.out.println("dav dav");
+    }
+
 }
